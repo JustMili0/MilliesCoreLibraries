49d6bf724816e003871b4f04bd53454a4c588b15
JustMili <rodx160@gmail.com>
2026-09-23 00:27:52 +0200

    Fix TagUtil

--- common/src/main/java/net/justmili/util/utils/common/TagUtil.java
+++ common/src/main/java/net/justmili/util/utils/common/TagUtil.java
@@ -1,9 +1,9 @@
 package net.justmili.util.utils.common;
 
-import net.justmili.servertweaks.ServerTweaks;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
@@ -13,27 +13,27 @@ import net.minecraft.world.level.block.Block;
 
 public class TagUtil {
 
-    public static <T> TagKey<T> create(ResourceKey<? extends Registry<T>> registries, String name) {
-        return TagKey.create(registries, ServerTweaks.asId(name));
+    public static <T> TagKey<T> create(ResourceKey<? extends Registry<T>> registries, ResourceLocation id) {
+        return TagKey.create(registries, id);
     }
 
-    public static TagKey<Block> block(String name) {
-        return create(Registries.BLOCK, name);
+    public static TagKey<Block> block(ResourceLocation id) {
+        return create(Registries.BLOCK, id);
     }
 
-    public static TagKey<Item> item(String name) {
-        return create(Registries.ITEM, name);
+    public static TagKey<Item> item(ResourceLocation id) {
+        return create(Registries.ITEM, id);
     }
 
-    public static TagKey<Biome> biome(String name) {
-        return create(Registries.BIOME, name);
+    public static TagKey<Biome> biome(ResourceLocation id) {
+        return create(Registries.BIOME, id);
     }
 
-    public static TagKey<EntityType<?>> entityType(String name) {
-        return create(Registries.ENTITY_TYPE, name);
+    public static TagKey<EntityType<?>> entityType(ResourceLocation id) {
+        return create(Registries.ENTITY_TYPE, id);
     }
 
-    public static TagKey<Enchantment> enchant(String name) {
-        return create(Registries.ENCHANTMENT, name);
+    public static TagKey<Enchantment> enchant(ResourceLocation id) {
+        return create(Registries.ENCHANTMENT, id);
     }
 }
